2a945c6738ae2a8e2bcb3fbfa9c8586330ed1165
Pablo Paez <pablopaez2307@gmail.com>
2022-03-08 13:02:30 -0300

    Subida a GitHub

---
+++ js/toast.js
@@ -0,0 +1,179 @@
+Toastify({
+
+    text: "Bienvenido",
+    
+    duration: 3000
+
+
+    
+    }).showToast();
+
+
+/*
+const boton_toast = document.getElementsByClassName("boton_toast");
+boton_toast.onclick = () => {
+ 
+    Toastify({
+        text: "Producto agregado",
+        duration: 3000
+        
+        }).showToast();
+}
+*/
+/*
+const boton_toast = document.getElementsByTagName('a')
+boton_toast.onclick = () => {
+ 
+    Toastify({
+        text: "Producto agregado",
+        duration: 3000
+        
+        }).showToast();
+}*/
+
+
+const boton_1 = document.getElementById("boton_1")
+boton_1.onclick = () => {
+ 
+    Toastify({
+        text: "Producto agregado: PC GAMER",
+        duration: 3000,
+        position: "center",
+        style:{
+            background: "rgb(22, 73, 156)",
+            color: "rgb(255, 255, 255)",
+            border: "1px solid lightblue",
+        }
+        
+        }).showToast();
+}
+
+const boton_2 = document.getElementById("boton_2")
+boton_2.onclick = () => {
+ 
+    Toastify({
+        text: "Producto agregado: PC GAMER (2)",
+        duration: 3000,
+        position: "center",
+        style:{
+            background: "rgb(22, 73, 156)",
+            color: "rgb(255, 255, 255)",
+            border: "1px solid lightblue",
+        }
+        
+        
+        }).showToast();
+}
+
+const boton_3 = document.getElementById("boton_3")
+boton_3.onclick = () => {
+ 
+    Toastify({
+        text: "Producto agregado: NOTEBOOK",
+        duration: 3000,
+        position: "center",
+        style:{
+            background: "rgb(22, 73, 156)",
+            color: "rgb(255, 255, 255)",
+            border: "1px solid lightblue",
+        }
+        
+        }).showToast();
+}
+
+
+
+const boton_4 = document.getElementById("boton_4")
+boton_4.onclick = () => {
+ 
+    Toastify({
+        text: "Producto agregado: DR STONE",
+        duration: 3000,
+        position: "center",
+        style:{
+            background: "rgb(22, 73, 156)",
+            color: "rgb(255, 255, 255)",
+            border: "1px solid lightblue",
+        }
+        
+        }).showToast();
+}
+
+const boton_5 = document.getElementById("boton_5")
+boton_5.onclick = () => {
+ 
+    Toastify({
+        text: "Producto agregado: CHAINSAW MAN",
+        duration: 3000,
+        position: "center",
+        style:{
+            background: "rgb(22, 73, 156)",
+            color: "rgb(255, 255, 255)",
+            border: "1px solid lightblue",
+        }
+        
+        }).showToast();
+}
+
+const boton_6 = document.getElementById("boton_6")
+boton_6.onclick = () => {
+ 
+    Toastify({
+        text: "Producto agregado: THE PROMISED NEVERLAND",
+        duration: 3000,
+        position: "center",
+        style:{
+            background: "rgb(22, 73, 156)",
+            color: "rgb(255, 255, 255)",
+            border: "1px solid lightblue",
+        }
+        
+        }).showToast();
+}
+
+const boton_7 = document.getElementById("boton_7")
+boton_7.onclick = () => {
+ 
+    Toastify({
+        text: "Producto agregado:LA DIVINA COMEDIA",
+        duration: 3000,
+        position: "center",
+        style:{
+            background: "rgb(22, 73, 156)",
+            color: "rgb(255, 255, 255)",
+            border: "1px solid lightblue",
+        }
+        
+        }).showToast();
+}
+
+const boton_8 = document.getElementById("boton_8")
+boton_8.onclick = () => {
+ 
+    Toastify({
+        text: "Producto agregado: COSMOS",
+        duration: 3000,
+        position: "center",
+        style:{
+            background: "rgb(22, 73, 156)",
+            color: "rgb(255, 255, 255)",
+            border: "1px solid lightblue",
+        }
+        
+        }).showToast();
+}
+
+const boton_9 = document.getElementById("boton_9")
+boton_9.onclick = () => {
+ 
+    Toastify({
+        text: "Producto agregado: COLECCION HP",
+        duration: 3000,position: "center",
+        style:{
+            background: "rgb(22, 73, 156)",
+            color: "rgb(255, 255, 255)",
+            border: "1px solid lightblue",
+        }
+        
+        }).showToast();
+}
